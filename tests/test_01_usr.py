629eae21456243ceb0d28578b2cf1deb4430614a
Saúl Correas Subías <saul.correas@gruposese.com>
2022-10-25 08:43:06 -0400

    Salvaguarda

--- tests/test_01_usr.py
+++ tests/test_01_usr.py
@@ -3,11 +3,9 @@ import datetime as dt
 import unittest
 import uuid
 
-import sqlalchemy.engine
-import sqlalchemy.schema
 
-import fixtures
 import wedge.api.usr as api_usr
+import wedge.core.engine as engine
 import wedge.model.ctl
 import wedge.model.ctl.ses as model_ses
 
@@ -17,20 +15,20 @@ class TestUsr(unittest.TestCase):
     def test_Insert(self):
         req = api_usr.UsrRequest()
         req.usr = wedge.model.ctl.Usr()
-        result = api_usr.getAction(self.ctl_md).Insert(self.ctl_con, req, self.session)
+        result = api_usr.getAction().Insert(self.ctl_con, req, self.session)
 
     def test_Update(self):
         req = api_usr.UsrRequest()
         req.data.usr = wedge.model.ctl.Usr()
-        result = api_usr.getAction(self.ctl_md).Insert(self.ctl_con, req, self.session)
+        result = api_usr.getAction().Insert(self.ctl_con, req, self.session)
 
     def test_Login(self):
-        action = wedge.api.usr.getAction(self.ctl_md)
+        action = api_usr.getAction()
         result = action.Login(self.ctl_con, "USUARIO", "123457")
         self.assertIsNone(result)
 
     def test_CheckSession(self):
-        sesDAL = model_ses.getDAL(self.ctl_md)
+        sesDAL = model_ses.SesDAL(engine.current_context.getCtlMetaData())
         ses = sesDAL.getEntity()
         ses.sescod = uuid.uuid4().hex
         ses.sesusrid = 1
@@ -39,23 +37,23 @@ class TestUsr(unittest.TestCase):
         ses.sesact = 1
         ses = sesDAL.Insert(self.ctl_con, ses)
 
-        action = wedge.api.usr.getAction(self.ctl_md)
-        ses1 = action.CheckSession(self.ctl_con, ses.sescod)
+        ses1 = api_usr.getAction().CheckSession(self.ctl_con, ses.sescod)
 
 
     @classmethod
     def setUpClass(cls) -> None:
-        
-        cls.ctl_eng = sqlalchemy.engine.create_engine(fixtures.CTL_URL)
-        cls.ctl_md = sqlalchemy.schema.MetaData(bind=cls.ctl_eng)
-        cls.ctl_con = cls.ctl_eng.connect()
 
-        cls.id_eng = sqlalchemy.engine.create_engine(fixtures.ID_URL)
-        cls.id_md = sqlalchemy.schema.MetaData(bind=cls.id_eng)
-        cls.id_con = cls.id_eng.connect()
+        engine.create_context("tests/config_test.ini")
+
+        # conexión a control
+        cls.ctl_con = engine.current_context.getCtlEngine().connect()
 
         # Genera la sesión para poder hacer todos los tests
-        cls.session = api_usr.getAction(cls.ctl_md).Login(cls.ctl_con, "USUARIO", "123456")
+        cls.session = api_usr.getAction().Login(cls.ctl_con, "USUARIO", "123456")
+        cls.session.ses.sesinsid = 1
+
+        cls.id_con = engine.current_context.getEngine(cls.session).connect()
+
 
     @classmethod
     def tearDownClass(cls) -> None:
