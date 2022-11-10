e3a2c7a23bcbfb76b53518ee6173ea82dd119c2b
Saúl Correas Subías <saul.correas@gruposese.com>
2022-11-10 15:21:51 -0500

    Salvaguarda

--- scripts/instance/m0000.sql
+++ scripts/instance/m0000.sql
@@ -1,4 +1,9 @@
 
+
+drop table if exists tca;
+drop table if exists tpa;
+drop table if exists tta;
+
 drop table if exists gdj;
 drop table if exists gdi;
 drop table if exists gcl;
@@ -8,6 +13,9 @@ drop table if exists gpb;
 drop table if exists gtz;
 drop table if exists gpa;
 
+drop table if exists ttc;
+drop table if exists ttm;
+drop table if exists ttp;
 drop table if exists usi;
 
 -------------------------------------------------
@@ -75,7 +83,9 @@ alter table gpb add constraint gpb_fk_02 foreign key(gpbgtzid) references gtz(gt
 create table gcl (
     gclid       bigserial primary key,
     gcleid      varchar(20) not null default '',
+    gclraz      varchar(80) not null default '',
     gclnom      varchar(80) not null default '',
+    gclnif      varchar(20) not null default '',
     gcldir      varchar(200) not null default '',
     gclcpo      varchar(20) not null default '',
     gclpob      varchar(80) not null default '',
@@ -92,7 +102,9 @@ create table gcl (
 comment on table gcl is         	'Clientes';
 comment on column gcl.gclid is		'Id. de cliente';
 comment on column gcl.gcleid is		'Código externo';
-comment on column gcl.gclnom is		'Nombre';
+comment on column gcl.gclraz is		'Razón social';
+comment on column gcl.gclnom is		'Nombre comercial';
+comment on column gcl.gclnif is		'NIF';
 comment on column gcl.gcldir is		'Dirección';
 comment on column gcl.gclcpo is		'Código postal';
 comment on column gcl.gclpob is		'Población';
@@ -117,7 +129,9 @@ alter table gcl add constraint gcl_fk_02 foreign key(gclgtzid) references gtz(gt
 create table gpr (
     gprid       bigserial primary key,
     gpreid      varchar(20) not null default '',
+    gprraz      varchar(80) not null default '',
     gprnom      varchar(80) not null default '',
+    gprnif      varchar(20) not null default '',
     gprdir      varchar(200) not null default '',
     gprcpo      varchar(20) not null default '',
     gprpob      varchar(80) not null default '',
@@ -134,7 +148,9 @@ create table gpr (
 comment on table gpr is         	'Clientes';
 comment on column gpr.gprid is		'Id. de cliente';
 comment on column gpr.gpreid is		'Código externo';
-comment on column gpr.gprnom is		'Nombre';
+comment on column gpr.gprraz is		'Razón social';
+comment on column gpr.gprnom is		'Nombre comercial';
+comment on column gpr.gprnif is		'NIF';
 comment on column gpr.gprdir is		'Dirección';
 comment on column gpr.gprcpo is		'Código postal';
 comment on column gpr.gprpob is		'Población';
@@ -171,6 +187,7 @@ create table gdi (
     gdipdc      varchar(80) not null default '',
     gdieml      text not null default '',
     gdioea      smallint not null default 0,
+    gdirmr      smallint not null default 0,
     gdiact      smallint not null default 0
 );
 
@@ -189,6 +206,7 @@ comment on column gdi.gditlf is		'Teléfono';
 comment on column gdi.gdipdc is		'Persona de contacto';
 comment on column gdi.gdieml is		'Correo electrónico';
 comment on column gdi.gdioea is		'Operador económico';
+comment on column gdi.gdirmr is		'Reserva de muelle requerida';
 comment on column gdi.gdiact is		'Activo/inactivo';
 
 create index gdi_ix_01 on gdi(gdigpacod);
@@ -221,3 +239,156 @@ create index gdj_ix_02 on gdj(gdjgclid);
 alter table gdj add constraint gdj_fk_01 foreign key(gdjgdiid) references gdi(gdiid);
 alter table gdj add constraint gdj_fk_02 foreign key(gdjgclid) references gcl(gclid);
 
+-------------------------------------------------
+-- Tipos de contenedores
+
+create table ttc (
+    ttccod      varchar(10) not null primary key,
+    ttcnom      varchar(80) not null default '',
+	ttclar		int not null default 0,
+	ttctar		numeric(9,3) not null default 0,
+	ttcpes		numeric(9,3) not null default 0,
+	ttccub		numeric(9,3) not null default 0,
+	ttclai		numeric(5,2) not null default 0,
+	ttcani		numeric(5,2) not null default 0,
+	ttcali		numeric(5,2) not null default 0,
+	ttcana		numeric(5,2) not null default 0,
+	ttcala		numeric(5,2) not null default 0
+);
+
+comment on table ttc is             'Tipos de contenedores';
+comment on column ttc.ttccod is     'Código';
+comment on column ttc.ttcnom is     'Descripción';
+comment on column ttc.ttclar is     'Largo en pies';
+comment on column ttc.ttctar is		'Tara';
+comment on column ttc.ttcpes is		'Peso carga útil';
+comment on column ttc.ttccub is		'Capacidad cúbica';
+comment on column ttc.ttclai is		'Largo interno';
+comment on column ttc.ttcani is 	'Ancho interno';
+comment on column ttc.ttcali is		'Alto interno';
+comment on column ttc.ttcana is		'Ancho apertura de puerta';
+comment on column ttc.ttcala is		'Alto apertura de puerta';
+
+insert into ttc values ('DRY20', 'DRY 20 PIES', 2300, 25000, 33.2, 5.9, 2.35, 2.39, 2.34, 2.28);
+insert into ttc values ('DRY40', 'DRY 40 PIES', 3750, 27600, 67.7, 12.03, 2.35, 2.39, 2.34, 2.28);
+
+-------------------------------------------------
+-- Tipos de mercancía
+
+create table ttm (
+    ttmcod      varchar(20) not null primary key,
+    ttmnom      varchar(80) not null default '',
+    ttmadr      smallint not null default 0,
+    ttmadrcls   varchar(10) not null default '',
+    ttmadrund   int not null default 0,
+    ttmadrflp   numeric(5,2) not null default 0,
+    ttmtmp      smallint not null default 0,
+    ttmtmpstp   numeric(5,2) not null default 0,
+    ttmtmpmin   numeric(5,2) not null default 0,
+    ttmtmpmax   numeric(5,2) not null default 0
+);
+
+comment on table ttm is             'Tipos de mercancías';
+comment on column ttm.ttmcod is     'Código';
+comment on column ttm.ttmnom is     'Descripción';
+comment on column ttm.ttmadr is     'ADR, mercancía peligrosa';
+comment on column ttm.ttmadrcls is  'ADR, clase';
+comment on column ttm.ttmadrund is  'ADR, número UN';
+comment on column ttm.ttmadrflp is  'ADR, punto inflamabilidad';
+comment on column ttm.ttmtmp is     'TMP, temperatura controlada';
+comment on column ttm.ttmtmpstp is  'TMP, set point';
+comment on column ttm.ttmtmpmin is  'TMP, mínimo';
+comment on column ttm.ttmtmpmax is  'TMP, máximo';
+
+-------------------------------------------------
+-- Tipos de paradas
+
+create table ttp (
+    ttpcod      varchar(5) not null primary key,
+    ttpnom      varchar(80) not null default '',
+	ttpcls		smallint not null default 0,
+	ttpsgn		smallint not null default 0,
+	ttpmin		smallint not null default 0,
+	ttpmax		smallint not null default 0
+);
+
+comment on table ttp is             'Tipos de paradas';
+comment on column ttp.ttpcod is     'Código';
+comment on column ttp.ttpnom is     'Descripción';
+comment on column ttp.ttpcls is     'Clase 0=Principal, 1=Auxiliar entrega, -1=Auxiliar retorno';
+comment on column ttp.ttpsgn is     'Signo 0=Neutral, 1=Incrementa, -1=Decrementa';
+comment on column ttp.ttpmin is     'Posición inicial válida [1,998]';
+comment on column ttp.ttpmax is     'Posición final válida [2,999]';
+
+-------------------------------------------------
+-- Cargas
+
+create table tca (
+    tcaid       bigserial primary key,
+    -- estado operativo y administrativo
+
+    -- datos identificativos
+    tcagclid    bigint not null default 0,
+    tcagcleid   varchar(20) not null default '',
+    tcagclraz   varchar(80) not null default '',
+    tcagclnom   varchar(80) not null default '',
+    tcagclnif   varchar(20) not null default '',
+    tcare1      varchar(20) not null default '',
+    tcare2      varchar(20) not null default '',
+    tcare3      varchar(20) not null default '',
+    tcare4      varchar(20) not null default '',
+
+    -- tipo de mercancía
+    tcattmcod   varchar(20) not null default '',
+    tcaadr      smallint not null default 0,
+    tcaadrcls   varchar(10) not null default '',
+    tcaadrund   int not null default 0,
+    tcaadrflp   numeric(5,2) not null default 0,
+    tcatmp      smallint not null default 0,
+    tcatmpstp   numeric(5,2) not null default 0,
+    tcatmpmin   numeric(5,2) not null default 0,
+    tcatmpmax   numeric(5,2) not null default 0,
+
+    -- contenedor
+    tcatcocod   varchar(20) not null default '',
+    tcattccod   varchar(10),
+
+    -- origen
+    tcaorgfec   date not null default '0001-01-01',
+    tcaorgeid   varchar(20) not null default '',
+    tcaorgnom   varchar(80) not null default '',
+    tcaorgpcp   varchar(20) not null default '',
+    tcaprgpob   varchar(80) not null default '',
+
+    -- destino
+    tcadesfec   date not null default '0001-01-01',
+    tcadeseid   varchar(20) not null default '',
+    tcadesnom   varchar(80) not null default '',
+    tcadespcp   varchar(20) not null default '',
+    tcadespob   varchar(80) not null default '',
+
+    -- dimensiones logísticas
+    tcadimpes   numeric(7, 3) not null default 0,
+    tcadimcub   numeric(7, 3) not null default 0,
+    tcadimplt   int not null default 0,
+    tcadimlar   numeric(7, 3) not null default 0,
+    tcadimalt   numeric(7, 3) not null default 0,
+    tcadimanc   numeric(7, 3) not null default 0
+
+);
+
+create index tca_ix_01 on tca(tcagclid);
+create index tca_ix_02 on tca(tcagcleid);
+create index tca_ix_03 on tca(tcagclraz);
+create index tca_ix_04 on tca(tcagclnom);
+create index tca_ix_05 on tca(tcagclnif);
+create index tca_ix_06 on tca(tcatcocod);
+
+alter table tca add constraint tca_fk_01 foreign key(tcagclid) references gcl(gclid);
+alter table tca add constraint tca_fk_06 foreign key(tcattccod) references ttc(ttccod);
+
+
+
+
+-------------------------------------------------
+-- Transportes
